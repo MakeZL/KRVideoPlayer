e9786885423cb41899600e24ab8369a2e0af8b11
MakeZL <120886865@qq.com>
2015-12-28 14:10:20 +0800

    增加showInView:的方法

--- Pod/Classes/KRVideoPlayerController.h
+++ Pod/Classes/KRVideoPlayerController.h
@@ -11,10 +11,13 @@
 @interface KRVideoPlayerController : MPMoviePlayerController
 
 @property (nonatomic, copy)void(^dimissCompleteBlock)(void);
+@property (nonatomic, copy)void(^fullScreenChangeCompleteBlock)(void);
+@property (nonatomic, copy)void(^shrinkScreenChangeCompleteBlock)(void);
 @property (nonatomic, assign) CGRect frame;
 
 - (instancetype)initWithFrame:(CGRect)frame;
 - (void)showInWindow;
+- (void)showInView:(UIView *)view;
 - (void)dismiss;
 
 @end
