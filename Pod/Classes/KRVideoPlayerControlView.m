42ed4358665c647b6fcbac616eb519edb129dcca
MakeZL <120886865@qq.com>
2015-12-28 14:11:11 +0800

    增加showInView:的方法实现,在屏幕旋转的时候增加了block回调通知外界

--- Pod/Classes/KRVideoPlayerControlView.m
+++ Pod/Classes/KRVideoPlayerControlView.m
@@ -1,246 +1,311 @@
 //
-//  KRVideoPlayerControlView.m
+//  KRVideoPlayerController.m
 //  KRKit
 //
 //  Created by aidenluo on 5/23/15.
 //  Copyright (c) 2015 36kr. All rights reserved.
 //
 
+#import "KRVideoPlayerController.h"
 #import "KRVideoPlayerControlView.h"
 
-static const CGFloat kVideoControlBarHeight = 40.0;
-static const CGFloat kVideoControlAnimationTimeinterval = 0.3;
-static const CGFloat kVideoControlTimeLabelFontSize = 10.0;
-static const CGFloat kVideoControlBarAutoFadeOutTimeinterval = 5.0;
+static const CGFloat kVideoPlayerControllerAnimationTimeinterval = 0.3f;
 
-@interface KRVideoPlayerControlView ()
+@interface KRVideoPlayerController ()
 
-@property (nonatomic, strong) UIView *topBar;
-@property (nonatomic, strong) UIView *bottomBar;
-@property (nonatomic, strong) UIButton *playButton;
-@property (nonatomic, strong) UIButton *pauseButton;
-@property (nonatomic, strong) UIButton *fullScreenButton;
-@property (nonatomic, strong) UIButton *shrinkScreenButton;
-@property (nonatomic, strong) UISlider *progressSlider;
-@property (nonatomic, strong) UIButton *closeButton;
-@property (nonatomic, strong) UILabel *timeLabel;
-@property (nonatomic, assign) BOOL isBarShowing;
-@property (nonatomic, strong) UIActivityIndicatorView *indicatorView;
+@property (nonatomic, strong) KRVideoPlayerControlView *videoControl;
+@property (nonatomic, strong) UIView *movieBackgroundView;
+@property (nonatomic, assign) BOOL isFullscreenMode;
+@property (nonatomic, assign) CGRect originFrame;
+@property (nonatomic, strong) NSTimer *durationTimer;
 
 @end
 
-@implementation KRVideoPlayerControlView
+@implementation KRVideoPlayerController
+
+- (void)dealloc
+{
+    [self cancelObserver];
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
+    self = [super init];
     if (self) {
-        self.backgroundColor = [UIColor clearColor];
-        [self addSubview:self.topBar];
-        [self.topBar addSubview:self.closeButton];
-        [self addSubview:self.bottomBar];
-        [self.bottomBar addSubview:self.playButton];
-        [self.bottomBar addSubview:self.pauseButton];
-        self.pauseButton.hidden = YES;
-        [self.bottomBar addSubview:self.fullScreenButton];
-        [self.bottomBar addSubview:self.shrinkScreenButton];
-        self.shrinkScreenButton.hidden = YES;
-        [self.bottomBar addSubview:self.progressSlider];
-        [self.bottomBar addSubview:self.timeLabel];
-        [self addSubview:self.indicatorView];
-        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
-        [self addGestureRecognizer:tapGesture];
+        self.view.frame = frame;
+        self.view.backgroundColor = [UIColor blackColor];
+        self.controlStyle = MPMovieControlStyleNone;
+        [self.view addSubview:self.videoControl];
+        self.videoControl.frame = self.view.bounds;
+        [self configObserver];
+        [self configControlAction];
     }
     return self;
 }
 
-- (void)layoutSubviews
+#pragma mark - Override Method
+
+- (void)setContentURL:(NSURL *)contentURL
 {
-    [super layoutSubviews];
-    self.topBar.frame = CGRectMake(CGRectGetMinX(self.bounds), CGRectGetMinY(self.bounds), CGRectGetWidth(self.bounds), kVideoControlBarHeight);
-    self.closeButton.frame = CGRectMake(CGRectGetWidth(self.topBar.bounds) - CGRectGetWidth(self.closeButton.bounds), CGRectGetMinX(self.topBar.bounds), CGRectGetWidth(self.closeButton.bounds), CGRectGetHeight(self.closeButton.bounds));
-    self.bottomBar.frame = CGRectMake(CGRectGetMinX(self.bounds), CGRectGetHeight(self.bounds) - kVideoControlBarHeight, CGRectGetWidth(self.bounds), kVideoControlBarHeight);
-    self.playButton.frame = CGRectMake(CGRectGetMinX(self.bottomBar.bounds), CGRectGetHeight(self.bottomBar.bounds)/2 - CGRectGetHeight(self.playButton.bounds)/2, CGRectGetWidth(self.playButton.bounds), CGRectGetHeight(self.playButton.bounds));
-    self.pauseButton.frame = self.playButton.frame;
-    self.fullScreenButton.frame = CGRectMake(CGRectGetWidth(self.bottomBar.bounds) - CGRectGetWidth(self.fullScreenButton.bounds), CGRectGetHeight(self.bottomBar.bounds)/2 - CGRectGetHeight(self.fullScreenButton.bounds)/2, CGRectGetWidth(self.fullScreenButton.bounds), CGRectGetHeight(self.fullScreenButton.bounds));
-    self.shrinkScreenButton.frame = self.fullScreenButton.frame;
-    self.progressSlider.frame = CGRectMake(CGRectGetMaxX(self.playButton.frame), CGRectGetHeight(self.bottomBar.bounds)/2 - CGRectGetHeight(self.progressSlider.bounds)/2, CGRectGetMinX(self.fullScreenButton.frame) - CGRectGetMaxX(self.playButton.frame), CGRectGetHeight(self.progressSlider.bounds));
-    self.timeLabel.frame = CGRectMake(CGRectGetMidX(self.progressSlider.frame), CGRectGetHeight(self.bottomBar.bounds) - CGRectGetHeight(self.timeLabel.bounds) - 2.0, CGRectGetWidth(self.progressSlider.bounds)/2, CGRectGetHeight(self.timeLabel.bounds));
-    self.indicatorView.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
+    [self stop];
+    [super setContentURL:contentURL];
+    [self play];
 }
 
-- (void)didMoveToSuperview
-{
-    [super didMoveToSuperview];
-    self.isBarShowing = YES;
-}
+#pragma mark - Publick Method
 
-- (void)animateHide
+- (void)showInWindow
 {
-    if (!self.isBarShowing) {
-        return;
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    if (!keyWindow) {
+        keyWindow = [[[UIApplication sharedApplication] windows] firstObject];
     }
-    [UIView animateWithDuration:kVideoControlAnimationTimeinterval animations:^{
-        self.topBar.alpha = 0.0;
-        self.bottomBar.alpha = 0.0;
+    [keyWindow addSubview:self.view];
+    self.view.alpha = 0.0;
+    [UIView animateWithDuration:kVideoPlayerControllerAnimationTimeinterval animations:^{
+        self.view.alpha = 1.0;
     } completion:^(BOOL finished) {
-        self.isBarShowing = NO;
+        
     }];
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
 }
 
-- (void)animateShow
-{
-    if (self.isBarShowing) {
-        return;
-    }
-    [UIView animateWithDuration:kVideoControlAnimationTimeinterval animations:^{
-        self.topBar.alpha = 1.0;
-        self.bottomBar.alpha = 1.0;
+- (void)showInView:(UIView *)view{
+    [view addSubview:self.view];
+    self.view.alpha = 0.0;
+    [UIView animateWithDuration:kVideoPlayerControllerAnimationTimeinterval animations:^{
+        self.view.alpha = 1.0;
     } completion:^(BOOL finished) {
-        self.isBarShowing = YES;
-        [self autoFadeOutControlBar];
+        
     }];
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
 }
 
-- (void)autoFadeOutControlBar
+- (void)dismiss
 {
-    if (!self.isBarShowing) {
-        return;
-    }
-    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(animateHide) object:nil];
-    [self performSelector:@selector(animateHide) withObject:nil afterDelay:kVideoControlBarAutoFadeOutTimeinterval];
-}
-
-- (void)cancelAutoFadeOutControlBar
-{
-    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(animateHide) object:nil];
-}
-
-- (void)onTap:(UITapGestureRecognizer *)gesture
-{
-    if (gesture.state == UIGestureRecognizerStateRecognized) {
-        if (self.isBarShowing) {
-            [self animateHide];
-        } else {
-            [self animateShow];
+    [self stop];
+    [self stopDurationTimer];
+    [UIView animateWithDuration:kVideoPlayerControllerAnimationTimeinterval animations:^{
+        self.view.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [self.view removeFromSuperview];
+        if (self.dimissCompleteBlock) {
+            self.dimissCompleteBlock();
         }
-    }
-}
-
-#pragma mark - Property
-
-- (UIView *)topBar
-{
-    if (!_topBar) {
-        _topBar = [UIView new];
-        _topBar.backgroundColor = [UIColor clearColor];
-    }
-    return _topBar;
-}
-
-- (UIView *)bottomBar
-{
-    if (!_bottomBar) {
-        _bottomBar = [UIView new];
-        _bottomBar.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.3];
-    }
-    return _bottomBar;
-}
-
-- (UIButton *)playButton
-{
-    if (!_playButton) {
-        _playButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_playButton setImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-play"]] forState:UIControlStateNormal];
-        _playButton.bounds = CGRectMake(0, 0, kVideoControlBarHeight, kVideoControlBarHeight);
-    }
-    return _playButton;
-}
-
-- (UIButton *)pauseButton
-{
-    if (!_pauseButton) {
-        _pauseButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_pauseButton setImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-pause"]] forState:UIControlStateNormal];
-        _pauseButton.bounds = CGRectMake(0, 0, kVideoControlBarHeight, kVideoControlBarHeight);
-    }
-    return _pauseButton;
-}
-
-- (UIButton *)fullScreenButton
-{
-    if (!_fullScreenButton) {
-        _fullScreenButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_fullScreenButton setImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-fullscreen"]] forState:UIControlStateNormal];
-        _fullScreenButton.bounds = CGRectMake(0, 0, kVideoControlBarHeight, kVideoControlBarHeight);
-    }
-    return _fullScreenButton;
-}
-
-- (UIButton *)shrinkScreenButton
-{
-    if (!_shrinkScreenButton) {
-        _shrinkScreenButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_shrinkScreenButton setImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-shrinkscreen"]] forState:UIControlStateNormal];
-        _shrinkScreenButton.bounds = CGRectMake(0, 0, kVideoControlBarHeight, kVideoControlBarHeight);
-    }
-    return _shrinkScreenButton;
-}
-
-- (UISlider *)progressSlider
-{
-    if (!_progressSlider) {
-        _progressSlider = [[UISlider alloc] init];
-        [_progressSlider setThumbImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-point"]] forState:UIControlStateNormal];
-        [_progressSlider setMinimumTrackTintColor:[UIColor whiteColor]];
-        [_progressSlider setMaximumTrackTintColor:[UIColor lightGrayColor]];
-        _progressSlider.value = 0.f;
-        _progressSlider.continuous = YES;
-    }
-    return _progressSlider;
-}
-
-- (UIButton *)closeButton
-{
-    if (!_closeButton) {
-        _closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_closeButton setImage:[UIImage imageNamed:[self videoImageName:@"kr-video-player-close"]] forState:UIControlStateNormal];
-        _closeButton.bounds = CGRectMake(0, 0, kVideoControlBarHeight, kVideoControlBarHeight);
-    }
-    return _closeButton;
-}
-
-- (UILabel *)timeLabel
-{
-    if (!_timeLabel) {
-        _timeLabel = [UILabel new];
-        _timeLabel.backgroundColor = [UIColor clearColor];
-        _timeLabel.font = [UIFont systemFontOfSize:kVideoControlTimeLabelFontSize];
-        _timeLabel.textColor = [UIColor whiteColor];
-        _timeLabel.textAlignment = NSTextAlignmentRight;
-        _timeLabel.bounds = CGRectMake(0, 0, kVideoControlTimeLabelFontSize, kVideoControlTimeLabelFontSize);
-    }
-    return _timeLabel;
-}
-
-- (UIActivityIndicatorView *)indicatorView
-{
-    if (!_indicatorView) {
-        _indicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
-        [_indicatorView stopAnimating];
-    }
-    return _indicatorView;
+    }];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
 }
 
 #pragma mark - Private Method
 
-- (NSString *)videoImageName:(NSString *)name
+- (void)configObserver
 {
-    if (name) {
-        NSString *path = [NSString stringWithFormat:@"KRVideoPlayer.bundle/%@",name];
-        return path;
-    }
-    return nil;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMPMoviePlayerPlaybackStateDidChangeNotification) name:MPMoviePlayerPlaybackStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMPMoviePlayerLoadStateDidChangeNotification) name:MPMoviePlayerLoadStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMPMoviePlayerReadyForDisplayDidChangeNotification) name:MPMoviePlayerReadyForDisplayDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMPMovieDurationAvailableNotification) name:MPMovieDurationAvailableNotification object:nil];
 }
+
+- (void)cancelObserver
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)configControlAction
+{
+    [self.videoControl.playButton addTarget:self action:@selector(playButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.pauseButton addTarget:self action:@selector(pauseButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.closeButton addTarget:self action:@selector(closeButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.fullScreenButton addTarget:self action:@selector(fullScreenButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.shrinkScreenButton addTarget:self action:@selector(shrinkScreenButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.progressSlider addTarget:self action:@selector(progressSliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.videoControl.progressSlider addTarget:self action:@selector(progressSliderTouchBegan:) forControlEvents:UIControlEventTouchDown];
+    [self.videoControl.progressSlider addTarget:self action:@selector(progressSliderTouchEnded:) forControlEvents:UIControlEventTouchUpInside];
+    [self.videoControl.progressSlider addTarget:self action:@selector(progressSliderTouchEnded:) forControlEvents:UIControlEventTouchUpOutside];
+    [self setProgressSliderMaxMinValues];
+    [self monitorVideoPlayback];
+}
+
+- (void)onMPMoviePlayerPlaybackStateDidChangeNotification
+{
+    if (self.playbackState == MPMoviePlaybackStatePlaying) {
+        self.videoControl.pauseButton.hidden = NO;
+        self.videoControl.playButton.hidden = YES;
+        [self startDurationTimer];
+        [self.videoControl.indicatorView stopAnimating];
+        [self.videoControl autoFadeOutControlBar];
+    } else {
+        self.videoControl.pauseButton.hidden = YES;
+        self.videoControl.playButton.hidden = NO;
+        [self stopDurationTimer];
+        if (self.playbackState == MPMoviePlaybackStateStopped) {
+            [self.videoControl animateShow];
+        }
+    }
+}
+
+- (void)onMPMoviePlayerLoadStateDidChangeNotification
+{
+    if (self.loadState & MPMovieLoadStateStalled) {
+        [self.videoControl.indicatorView startAnimating];
+    }
+}
+
+- (void)onMPMoviePlayerReadyForDisplayDidChangeNotification
+{
+    
+}
+
+- (void)onMPMovieDurationAvailableNotification
+{
+    [self setProgressSliderMaxMinValues];
+}
+
+- (void)playButtonClick
+{
+    [self play];
+    self.videoControl.playButton.hidden = YES;
+    self.videoControl.pauseButton.hidden = NO;
+}
+
+- (void)pauseButtonClick
+{
+    [self pause];
+    self.videoControl.playButton.hidden = NO;
+    self.videoControl.pauseButton.hidden = YES; 
+}
+
+- (void)closeButtonClick
+{
+    [self dismiss];
+}
+
+- (void)fullScreenButtonClick
+{
+    if (self.isFullscreenMode) {
+        return;
+    }
+    if (self.fullScreenChangeCompleteBlock) {
+        self.fullScreenChangeCompleteBlock();
+    }
+    self.originFrame = self.view.frame;
+    CGFloat height = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat width = [[UIScreen mainScreen] bounds].size.height;
+    CGRect frame = CGRectMake((height - width) / 2, (width - height) / 2, width, height);;
+    [UIView animateWithDuration:0.3f animations:^{
+        self.frame = frame;
+        [self.view setTransform:CGAffineTransformMakeRotation(M_PI_2)];
+    } completion:^(BOOL finished) {
+        self.isFullscreenMode = YES;
+        self.videoControl.fullScreenButton.hidden = YES;
+        self.videoControl.shrinkScreenButton.hidden = NO;
+    }];
+}
+
+- (void)shrinkScreenButtonClick
+{
+    if (!self.isFullscreenMode) {
+        return;
+    }
+    if (self.shrinkScreenChangeCompleteBlock) {
+        self.shrinkScreenChangeCompleteBlock();
+    }
+    [UIView animateWithDuration:0.3f animations:^{
+        [self.view setTransform:CGAffineTransformIdentity];
+        self.frame = self.originFrame;
+    } completion:^(BOOL finished) {
+        self.isFullscreenMode = NO;
+        self.videoControl.fullScreenButton.hidden = NO;
+        self.videoControl.shrinkScreenButton.hidden = YES;
+    }];
+}
+
+- (void)setProgressSliderMaxMinValues {
+    CGFloat duration = self.duration;
+    self.videoControl.progressSlider.minimumValue = 0.f;
+    self.videoControl.progressSlider.maximumValue = duration;
+}
+
+- (void)progressSliderTouchBegan:(UISlider *)slider {
+    [self pause];
+    [self.videoControl cancelAutoFadeOutControlBar];
+}
+
+- (void)progressSliderTouchEnded:(UISlider *)slider {
+    [self setCurrentPlaybackTime:floor(slider.value)];
+    [self play];
+    [self.videoControl autoFadeOutControlBar];
+}
+
+- (void)progressSliderValueChanged:(UISlider *)slider {
+    double currentTime = floor(slider.value);
+    double totalTime = floor(self.duration);
+    [self setTimeLabelValues:currentTime totalTime:totalTime];
+}
+
+- (void)monitorVideoPlayback
+{
+    double currentTime = floor(self.currentPlaybackTime);
+    double totalTime = floor(self.duration);
+    [self setTimeLabelValues:currentTime totalTime:totalTime];
+    self.videoControl.progressSlider.value = ceil(currentTime);
+}
+
+- (void)setTimeLabelValues:(double)currentTime totalTime:(double)totalTime {
+    double minutesElapsed = floor(currentTime / 60.0);
+    double secondsElapsed = fmod(currentTime, 60.0);
+    NSString *timeElapsedString = [NSString stringWithFormat:@"%02.0f:%02.0f", minutesElapsed, secondsElapsed];
+    
+    double minutesRemaining = floor(totalTime / 60.0);;
+    double secondsRemaining = floor(fmod(totalTime, 60.0));;
+    NSString *timeRmainingString = [NSString stringWithFormat:@"%02.0f:%02.0f", minutesRemaining, secondsRemaining];
+    
+    self.videoControl.timeLabel.text = [NSString stringWithFormat:@"%@/%@",timeElapsedString,timeRmainingString];
+}
+
+- (void)startDurationTimer
+{
+    self.durationTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(monitorVideoPlayback) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.durationTimer forMode:NSDefaultRunLoopMode];
+}
+
+- (void)stopDurationTimer
+{
+    [self.durationTimer invalidate];
+}
+
+- (void)fadeDismissControl
+{
+    [self.videoControl animateHide];
+}
+
+#pragma mark - Property
+
+- (KRVideoPlayerControlView *)videoControl
+{
+    if (!_videoControl) {
+        _videoControl = [[KRVideoPlayerControlView alloc] init];
+    }
+    return _videoControl;
+}
+
+- (UIView *)movieBackgroundView
+{
+    if (!_movieBackgroundView) {
+        _movieBackgroundView = [UIView new];
+        _movieBackgroundView.alpha = 0.0;
+        _movieBackgroundView.backgroundColor = [UIColor blackColor];
+    }
+    return _movieBackgroundView;
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [self.view setFrame:frame];
+    [self.videoControl setFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+    [self.videoControl setNeedsLayout];
+    [self.videoControl layoutIfNeeded];
+}
+
 
 @end
